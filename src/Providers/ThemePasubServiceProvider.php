25895fbd7c8257cb0f4114d7bfdd760a38bb8591
psubat <p.subat@ecomsilio.de>
2017-04-03 16:39:01 +0200

    changed things

--- src/Providers/ThemePasubServiceProvider.php
+++ src/Providers/ThemePasubServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ThemePasub\Providers;
+namespace CeresHipster\Providers;
 
 use IO\Extensions\Functions\Partial;
 use IO\Helper\CategoryKey;
@@ -10,26 +10,28 @@ use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ConfigRepository;
+use IO\Helper\ComponentContainer;
 
-class ThemePasubServiceProvider extends ServiceProvider
+
+class CeresHipsterServiceProvider extends ServiceProvider
 {
+    const EVENT_LISTENER_PRIORITY = 99;
+    /**
+     * Register the service provider.
+     */
 
-	/**
-	 * Register the service provider.
-	 */
-	public function register(){}
+    public function register() {
 
-	/**
- * Boot a template for the footer that will be displayed in the template plugin instead of the original footer.
- */
-	public function boot(Twig $twig, Dispatcher $eventDispatcher)
-	{
+    }
 
-		$eventDispatcher->listen('IO.init.templates', function (Partial $partial) {
-				$partial->set('head', 'ThemePasub::PageDesign.Partials.Head');
-				//$partial->set('header', 'ThemePasub::PageDesign.Partials.Header.Header');
-				$partial->set('footer', 'ThemePasub::PageDesign.Partials.ThemeFooter');
-				$partial->set('page-design', 'ThemePasub::PageDesign.PageDesign');
-			}, 1);
-	}
+    public function boot(Twig $twig, Dispatcher $eventDispatcher)
+    {
+        $eventDispatcher->listen('IO.init.templates', function (Partial $partial) {
+					$partial->set('head', 'ThemePasub::PageDesign.Partials.Head');
+					//$partial->set('header', 'ThemePasub::PageDesign.Partials.Header.Header');
+					$partial->set('footer', 'ThemePasub::PageDesign.Partials.ThemeFooter');
+					$partial->set('page-design', 'ThemePasub::PageDesign.PageDesign');
+
+        }, self::EVENT_LISTENER_PRIORITY);
+    }
 }
